1ff954176d3c475f811194f6abaea7ad2b487931
Dig <Dig03@users.noreply.github.com>
2019-07-23 20:41:49 +0100

    Change socket name to something more sane.

--- server.py
+++ server.py
@@ -5,23 +5,25 @@ import socket
 CFG = ('', 413)
 MAX_CONNECTIONS = 100
 
-iface = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-iface.bind(CFG)
-iface.listen(MAX_CONNECTIONS)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(CFG)
+s.listen(MAX_CONNECTIONS)
 print("Running.")
 
 clients = []
 
+
 def main():
     try:
         while True:
-            client_iface, addr = iface.accept()
+            client_iface, addr = s.accept()
             print("Accepted {}.".format(addr))
             clients.append(client_iface)
             message_clients(clients, "{} joined!".format(addr))
     except KeyboardInterrupt:
         # needs threading, right now is blocked until new client connects -R
-        iface.close()
+        s.close()
 
 
 def message_clients(clients, message):
