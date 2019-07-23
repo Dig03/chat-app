9d96ff392a1b8297323fe0e8a3c13f3042926433
Dig <Dig03@users.noreply.github.com>
2019-07-23 21:13:17 +0100

    Made server fancier and threaded.

--- server.py
+++ server.py
@@ -1,35 +1,73 @@
-# Simple broadcast server.
-
 import socket
-
-CFG = ('', 413)
-MAX_CONNECTIONS = 100
+import threading
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(CFG)
-s.listen(MAX_CONNECTIONS)
-print("Running.")
+class Server:
+    """
+    Relatively simple threaded chat server.
+    """
 
-clients = []
+    """
+    Arguments:
+        host: interface IP for server to bind to
+        port: port to bind to
+        max_connections: maximum number of possible accepted connections
+        bufsize: maximum buffer for a single received message
+        timeout: time to wait (in seconds) for an operation on a client to complete
+    """
+    def __init__(self, host, port, max_connections = 100, bufsize = 1024, timeout = 60):
+        self.host = host
+        self.port = port
+        self.max_connections = max_connections
+        self.bufsize = bufsize
+        self.timeout = timeout
+        self.clients = {}
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((host, port))
 
+    def _register_client(self, client, address):
+        self.clients[address] = client
 
-def main():
-    try:
+    """
+    Transmit a message to all clients except those in exclude.
+    
+    Arguments:
+        message: bytes object to broadcast.
+        exclude: a list of addresses to not broadcast to.
+    """
+    def _broadcast(self, message, exclude=None):
+        if exclude is None:
+            exclude = []
+        for address in self.clients:
+            if address not in exclude:
+                self.clients[address].send(message + '\n')
+
+    def listen(self):
+        self.sock.listen(self.max_connections)
+        print("Listening on {}:{}.".format(self.host, self.port))
         while True:
-            client_iface, addr = s.accept()
-            print("Accepted {}.".format(addr))
-            clients.append(client_iface)
-            message_clients(clients, "{} joined!".format(addr))
-    except KeyboardInterrupt:
-        # needs threading, right now is blocked until new client connects -R
-        s.close()
+            client, address = self.sock.accept()
+            print("Received connection from {}.".format(address))
+            client.settimeout(self.timeout)
+            self._register_client(client, address)
+            threading.Thread(target=self.client_loop, args=(client, address)).start()
 
-
-def message_clients(clients, message):
-    for client in clients:
-        client.send(message.encode())
+    def client_loop(self, client, address):
+        while True:
+            try:
+                data = client.recv(self.bufsize)
+                if data:
+                    formatted = "{}: {}".format(address, data)
+                    print(formatted)
+                    self._broadcast(formatted.encode(), [address])
+                else:
+                    raise socket.error("Client disconnected.")
+            except (socket.timeout, socket.error):
+                print("{} dropped.".format(address))
+                client.close()
+                return False
 
 
 if __name__ == '__main__':
-    main()
+    Server('', 413).listen()
